dd079a1aaacf1865788024880c0002ce35161c06
Greg Coppola <greg@hiro.so>
2022-07-26 10:49:17 -0500

    now counting the marf reads

--- analysis/analyze1.py
+++ analysis/analyze1.py
@@ -8,12 +8,19 @@ lines = open(fname).readlines()
 print('lines', len(lines))
 
 
+curr_tx = 0
+marf_reads_this_tx = 0
 for line in lines:
     if "MarfConnection::get" in line:
+        marf_reads_this_tx += 1
         pass
     elif "build_anchored_block delta" in line:
         pass
     elif "candidate delta tx" in line:
+        print('curr_tx', curr_tx)
+        print('marf_reads_this_tx', marf_reads_this_tx)
+        marf_reads_this_tx = 0
+        curr_tx += 1
         pass
     else:
         print('line not recognized:', line)
