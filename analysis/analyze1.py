b9604c55102d1e76ca907f94b05d6f3bd7dba90c
Greg Coppola <greg@hiro.so>
2022-07-26 10:58:35 -0500

    added in time part

--- analysis/analyze1.py
+++ analysis/analyze1.py
@@ -17,10 +17,17 @@ for line in lines:
     elif "build_anchored_block delta" in line:
         pass
     elif "candidate delta tx" in line:
-        print('curr_tx', curr_tx)
-        print('marf_reads_this_tx', marf_reads_this_tx)
+        time_parts = line.split('time_cost=')
+        if len(time_parts) > 1:
+            time_part = time_parts[1]
+            print('curr_tx', curr_tx, 'marf_reads_this_tx', marf_reads_this_tx, 'time_part', time_part)
+        else:
+            print('error on line:', line)
+
+        # Do after printing
         marf_reads_this_tx = 0
         curr_tx += 1
+
         pass
     else:
         print('line not recognized:', line)
